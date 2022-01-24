6ac98cb6709409d78591cffa78186cfb233b5e6e
Camilo Castillo <camilo.castillo@blackboard.com>
2022-01-24 10:06:32 -0500

    Fixing issues with timesptamps

--- main.py
+++ main.py
@@ -5,7 +5,9 @@
 import requests
 from json import loads
 from datetime import datetime
-from dateutil import tz
+from dateutil import tz, parser
+
+# Constants
 token = "Basic OTpzaTNlV2M3b0FwL1pwaVgyaU80L0tvRHg2emNXNnVmM2lOa29YMDFvNU5jPQ=="
 activities_url = "https://api.slangapp.com/challenges/v1/activities"
 sessions_url = "https://api.slangapp.com/challenges/v1/activities/sessions"
@@ -13,23 +15,51 @@ sessions_url = "https://api.slangapp.com/challenges/v1/activities/sessions"
 def calculate_users_sessions():
     user_sessions = {}
     users_timeline = {}
-    # Use a breakpoint in the code line below to debug your script.
+    # First get the list of activities.
     activities_resp = requests.get(activities_url, headers={'Content-Type': 'application/json', "Authorization": token})
     assert activities_resp, "Bad response [{}] from sessions url".format(activities_resp.status_code)
     activities_list = loads(activities_resp.content)["activities"]
+
+
     for activitie in activities_list:
+
+        # Transform dato to time stamps
         answered_ts = isotots(activitie["answered_at"])
         first_seen_ts = isotots(activitie["first_seen_at"])
-        if activitie["user_id"] in user_sessions :
+
+        # The user will be created in the list if this one dont exist in the dict
+        if activitie["user_id"] in user_sessions:
+
+            # Make a binary search to find if the new activity was perform inside an existing period.
             position = search_user_session(users_timeline[activitie["user_id"]], (first_seen_ts, answered_ts))
             if position[0] == "inside":
-                new_end_time = activitie["answered_at"] if user_sessions[activitie["user_id"]]["ended_at"] < activitie["answered_at"] else user_sessions[activitie["user_id"]]["ended_at"]
-                new_start_time = activitie["first_seen_at"] if user_sessions[activitie["user_id"]]["started_at"] < activitie["first_seen_at"] else user_sessions[activitie["user_id"]]["started_at"]
-                user_sessions[activitie["user_id"]]["ended_at"] = new_end_time
-                user_sessions[activitie["user_id"]]["started_at"] = new_start_time
-                user_sessions[activitie["user_id"]]["duration"] = new_start_time
-                user_sessions[activitie["user_id"]]["activity_ids"].push(activitie["id"]),
-                users_timeline[activitie["user_id"]] = [(new_start_time, new_end_time)]
+
+                # Original time session
+                session_answered_time = user_sessions[activitie["user_id"]][position[1]]["ended_at"]
+                session_first_seen_time = user_sessions[activitie["user_id"]][position[1]]["started_at"]
+                session_answered_ts = isotots(session_answered_time)
+                session_first_seen_ts = isotots(session_first_seen_time)
+
+                # Update set the new older and news date on the period
+                new_end_time = session_answered_time
+                new_end_ts = answered_ts
+                if session_answered_ts < answered_ts:
+                    new_end_time = activitie["answered_at"]
+                    new_end_ts = session_answered_ts
+
+                new_start_time = session_answered_time
+                new_start_ts = answered_ts
+                if session_first_seen_ts > first_seen_ts:
+                    new_start_time = activitie["first_seen_at"]
+                    new_start_ts = first_seen_ts
+
+                #Set the new values inside the period
+                selected_session = user_sessions[activitie["user_id"]][position[1]]
+                selected_session["ended_at"] = new_end_time
+                selected_session["started_at"] = new_start_time
+                selected_session["duration"] = new_start_time
+                selected_session["activity_ids"].append(activitie["id"]),
+                users_timeline[activitie["user_id"]] = [(new_end_ts, new_start_ts)]
             else:
                 new_session = {
                     "ended_at": activitie["answered_at"],
@@ -37,7 +67,7 @@ def calculate_users_sessions():
                     "activity_ids": [activitie["id"]],
                     "duration": answered_ts - first_seen_ts,
                 }
-                user_sessions[activitie["user_id"]] = user_sessions[activitie["user_id"]][:position[1]] + new_session + user_sessions[activitie["user_id"]][position[1]:]
+                user_sessions[activitie["user_id"]] = user_sessions[activitie["user_id"]][:position[1]] + [new_session] + user_sessions[activitie["user_id"]][position[1]:]
 
         else:
             user_sessions[activitie["user_id"]] = [
@@ -50,11 +80,11 @@ def calculate_users_sessions():
             ]
             users_timeline[activitie["user_id"]] = [(first_seen_ts, answered_ts)]
 
-
-    session_resp = requests.post(sessions_url, json={"user_session": user_sessions}, headers={'Content-Type': 'application/json', "Authorization": token})
+    #Push the session list into the enpoint
+    session_resp = requests.post(sessions_url, json={"user_sessions": user_sessions}, headers={'Content-Type': 'application/json', "Authorization": token})
     assert session_resp, "Bad response [{}] from sessions url".format(session_resp.status_code)
 
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print("successfully uploaded")
 
 def search_user_session(session_timeline, activity_period):
     left = 0
@@ -67,7 +97,7 @@ def search_user_session(session_timeline, activity_period):
         elif position == "left":
             right = mid - 1
         else:
-            left + 1
+            left = mid + 1
     return position, mid
 
 def is_period_outside_session(session_period, activity_period):
@@ -81,7 +111,7 @@ def is_period_outside_session(session_period, activity_period):
     return ret
 
 def isotots(isostr, fmt='%Y-%m-%dT%H:%M:%S.%fZ'):
-    return datetime.strptime(isostr, fmt).replace(tzinfo=tz.tzutc()).timestamp()
+    return parser.parse(isostr).timestamp()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
